7cd5534e63f934674c8343dae4bb36cfd74ddfe6
Amrutha Rao <arr2249@columbia.edu>
2026-06-13 13:09:24 -0400

    preliminary homepage

---
+++ frontend/app/profile/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+import Navbar from "@/components/Navbar"
+
+
+export default function Profile() {
+  return (
+    <div className="flex flex-col min-h-screen bg-stone-200">
+      <div>
+        Name
+      </div>
+      <div>
+        Lives
+      </div>
+    </div>
+    
+  );
+}
--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -1,4 +1,6 @@
+"use client"
 import { User, Home, MessageCircle } from "lucide-react"
+
 export default function Navbar() {
     return (
       <div className="fixed bottom-0 left-0 right-0 flex justify-around items-center h-16 border-t bg-[#c4956a]">
